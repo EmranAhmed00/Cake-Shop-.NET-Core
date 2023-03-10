d71f7f7b99856b8508be8d26bd8219755c2769f0
Emran-Web <emran.ahmed.dev@gmail.com>
2023-03-10 21:29:43 +0100

    Add project files.

---
+++ Cake_Shop_Thelins/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Cake_Shop_Thelins.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Cake_Shop_Thelins.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
